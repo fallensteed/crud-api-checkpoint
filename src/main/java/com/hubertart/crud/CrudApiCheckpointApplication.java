fbb7d2f9b4115aae4154fc1fe2653ab3a976dac8
fallensteed <scott@hubertart.com>
2020-12-11 10:39:14 -0700

    Initial commit

---
+++ src/main/java/com/hubertart/crud/CrudApiCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.hubertart.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiCheckpointApplication.class, args);
+	}
+
+}
